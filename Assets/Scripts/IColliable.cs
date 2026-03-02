f0902ad0a20d3c46a2596fe5a2bf18a8fb3591d8
HERVY PAYUMO <hervyp@live.wsd1.org>
2026-03-02 13:35:19 -0600

    test

--- Assets/Scripts/IColliable.cs
+++ Assets/Scripts/IColliable.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class IColliable : MonoBehaviour
+public interface ICollidable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void OnCollision(PlayerMovement player);
 }
