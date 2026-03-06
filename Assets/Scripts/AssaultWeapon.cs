d7c0869c1f58bdb6c1539b57c09c37558b39ae0f
Arief Ocampo <ariefocampo@gmail.com>
2026-03-06 13:54:40 -0600

    gun is now fixed and working

--- Assets/Scripts/AssaultWeapon.cs
+++ Assets/Scripts/AssaultWeapon.cs
@@ -5,29 +5,22 @@ public class AssaultWeaponOriginal : MonoBehaviour, IBullet
     public GameObject Bullet;
     public float BulletPower = 6f;
 
-    public void Shoot(float BulletPower)
+    void IBullet.Shoot(float BulletPower)
     {
         GameObject Bullet = Instantiate(this.Bullet, transform.position, transform.rotation); //Clones the bullet
         Rigidbody rb = Bullet.GetComponent<Rigidbody>(); //Rigidbody so the bullet can move
         rb.AddForce(-transform.forward * BulletPower, ForceMode.Impulse); //bullet launching
     }
 
-    public void PistolShoot(float BulletPower1)
-    {
-        GameObject Bullet = Instantiate(this.Bullet, transform.position, transform.rotation);
-        Rigidbody rb = Bullet.GetComponent<Rigidbody>();
-        rb.AddForce(-transform.forward * BulletPower1, ForceMode.Impulse);
-    }
 
     void Update()
     {
         IBullet iBullet = GetComponent<IBullet>();
-
         if (Input.GetButtonDown("Fire1"))
         {
-          
+
             iBullet.Shoot(BulletPower);
         }
-        
+
     }
 }
--- Assets/Scripts/PistolWeapon.cs
+++ Assets/Scripts/PistolWeapon.cs
@@ -4,17 +4,14 @@ public class PistolWeapon : MonoBehaviour, IBullet
 {
     public GameObject Bullet;
     public float BulletPower = 6f;
-    public void PistolShoot(float BulletPower1)
+
+    void IBullet.Shoot(float PistolShoot)
     {
         GameObject Bullet = Instantiate(this.Bullet, transform.position, transform.rotation);  //Clones the bullet
         Rigidbody rb = Bullet.GetComponent<Rigidbody>(); //Rigidbody so the bullet can move
-        rb.AddForce(-transform.forward * BulletPower1, ForceMode.Impulse); //bullet launching
-    }
-    public void Shoot(float BulletPower)
-    {
-         
-    }
+        rb.AddForce(-transform.forward * BulletPower, ForceMode.Impulse); //bullet launching
 
+    }
 
     void Update()
     {
@@ -22,7 +19,7 @@ public class PistolWeapon : MonoBehaviour, IBullet
 
         if (Input.GetButtonDown("Fire2"))
         {
-            iBullet.PistolShoot(BulletPower);
+            iBullet.Shoot(BulletPower);
         }
     }
 }
