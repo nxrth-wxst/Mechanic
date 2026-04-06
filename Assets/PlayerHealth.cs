b77cfa884e9a39b7977bf4d6b71d0143a30ef425
arief <ariefocampo@gmail.com>
2026-04-05 19:53:44 -0500

    healthbar

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,14 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour, PColliable
 {
-    [SerializeField] private float playerHealth = 10f;
-    
-    
-    void PColliable.PlayerCollision(EnemyDamage enemyDamage)
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float enemyContact = 10f;
+    [SerializeField] private Slider healthSlider;
+
+    private float currentHealth;
+
+    private void Start()
     {
-        playerHealth -= 1f;
+        currentHealth = maxHealth;
+        UpdateSlider();
     }
 
-    
+    public void PlayerCollision(EnemyDamage enemy)
+    {
+        TakeDamage(enemyContact);
+    }
+
+    private void TakeDamage(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - amount, 0f, maxHealth); UpdateSlider();
+
+        if (currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void UpdateSlider()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth / maxHealth;
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("dead");
+    }
 }
