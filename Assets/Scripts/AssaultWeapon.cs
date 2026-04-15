d705efcc19600bb97ae208f11d5d78fbb46e500c
Arief Ocampo <ariefocampo@gmail.com>
2026-04-15 15:27:46 -0500

    PROGRESSION

--- Assets/Scripts/AssaultWeapon.cs
+++ Assets/Scripts/AssaultWeapon.cs
@@ -6,29 +6,38 @@ public class AssaultWeaponOriginal : MonoBehaviour, IBullet
     [SerializeField] private GameObject bullet;
     [SerializeField] private float bulletpower = 6f;
     private Controls controls;
+    private bool isShooting = false;
 
-   
     void Awake()
     {
         controls = new Controls();
     }
-
+    
     void OnEnable()
     {
+  
         controls.Player.Enable();
-        controls.Player.ShootAssault.performed += ShootTheGun;
+        controls.Player.ShootAssault.started += ShootTheGun;
+        controls.Player.ShootAssault.canceled -= ShootTheGun;
     }
-
-   
     void OnDisable()
     {
-        controls.Player.ShootAssault.performed -= ShootTheGun;
+        controls.Player.ShootAssault.started += ShootTheGun;
+        controls.Player.ShootAssault.canceled -= ShootTheGun;
         controls.Player.Disable();
     }
 
+    private void OnShootStarted(InputAction.CallbackContext context)
+    {
+        isShooting = true;
+    }
+
+    private void OnShootCanceled(InputAction.CallbackContext context)
+    {
+        isShooting = false;
+    }
     private void ShootTheGun(InputAction.CallbackContext context)
     {
-        // Interface check and execution
         IBullet iBullet = GetComponent<IBullet>();
         if (iBullet != null)
         {
@@ -49,4 +58,18 @@ public class AssaultWeaponOriginal : MonoBehaviour, IBullet
             rb.AddForce(-transform.forward * BulletPower, ForceMode.Impulse);
         }
     }
+
+    private void Update()
+    {
+        if (isShooting)
+        {
+            IBullet iBullet = GetComponent<IBullet>();
+            if (iBullet != null)
+            {
+                iBullet.Shoot(bulletpower);
+            }
+        }
+
+    }
+
 }
