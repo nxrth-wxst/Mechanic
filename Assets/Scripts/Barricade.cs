490957a2656921c59a50fae5f302741377a0f764
HERVY PAYUMO <hervyp@live.wsd1.org>
2026-04-20 13:59:25 -0500

    Wdadssa

--- Assets/Scripts/Barricade.cs
+++ Assets/Scripts/Barricade.cs
@@ -1,16 +1,122 @@
 using UnityEngine;
-
+using System;
+using System.Collections.Generic;
+[RequireComponent(typeof(Collider))]
 public class Barricade : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public static event EventHandler<BoardDamagedEventArgs> OnBoardDamaged;
+    public static event EventHandler<BoardRepairedEventArgs> OnBoardRepaired;
+    public static event EventHandler<BarricadeEventArgs> OnBoardBroken;
+    public static event EventHandler<BarricadeBrokenEventArgs> OnBarricadeBroken;
+
+    [Header("Board Config")]
+    [SerializeField] private int boardCount = 4;
+    [SerializeField] private float boardMaxHealth = 100f;
+
+    [Header("Visual Boards")]
+    [Tooltip("Assign the GameObject for each board plank in order")]
+    [SerializeField] private List<GameObject> boardVisuals = new();
+
+    private List<Board> _boards = new();
+    private bool _isBroken;
+
+    public bool IsBroken => _isBroken;
+    public int BoardCount => _boards.Count;
+    public int IntactBoards => _boards.FindAll(b => !b.IsBroken).Count;
+
+
+    private void Awake()
     {
-        
+        InitialiseBoards();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitialiseBoards()
     {
+        _boards.Clear();
+        for (int i = 0; i < boardCount; i++)
+            _boards.Add(new Board(boardMaxHealth));
+        _isBroken = false;
+    }
+
+
+    public void DamageBoard(float damage)
+    {
+        if (_isBroken) return;
+
+        int index = GetFirstIntactBoardIndex();
+        if (index < 0) return;
+
+        Board board = _boards[index];
+        float before = board.CurrentHealth;
+        float actual = board.TakeDamage(damage);
+
+        if (actual <= 0f) return;
+
         
+        OnBoardDamaged?.Invoke(this,
+            new BoardDamagedEventArgs(index, this, before, board.CurrentHealth));
+
+        
+        if (board.IsBroken)
+        {
+            SetBoardVisual(index, false);
+            OnBoardBroken?.Invoke(this, new BarricadeEventArgs(index, this));
+
+           
+            if (IntactBoards == 0)
+            {
+                _isBroken = true;
+                GetComponent<Collider>().enabled = false; 
+                OnBarricadeBroken?.Invoke(this,
+                    new BarricadeBrokenEventArgs(index, this, transform.position));
+            }
+        }
+    }
+
+
+    public bool RepairBoard(int boardIndex)
+    {
+        if (boardIndex < 0 || boardIndex >= _boards.Count) return false;
+
+        Board board = _boards[boardIndex];
+        if (!board.IsBroken) return false;
+
+        float restored = board.Repair(board.MaxHealth); 
+        bool fullRepair = restored > 0f;
+
+        if (fullRepair)
+        {
+            _isBroken = false;
+            GetComponent<Collider>().enabled = true;
+            SetBoardVisual(boardIndex, true);
+
+            OnBoardRepaired?.Invoke(this,
+                new BoardRepairedEventArgs(boardIndex, this, restored, true));
+        }
+
+        return fullRepair;
+    }
+
+    public void ResetBarricade()
+    {
+        InitialiseBoards();
+        for (int i = 0; i < boardVisuals.Count; i++)
+            SetBoardVisual(i, true);
+        GetComponent<Collider>().enabled = true;
+    }
+
+    private int GetFirstIntactBoardIndex()
+    {
+        for (int i = 0; i < _boards.Count; i++)
+            if (!_boards[i].IsBroken) return i;
+        return -1;
+    }
+
+    private void SetBoardVisual(int index, bool visible)
+    {
+        if (index < boardVisuals.Count && boardVisuals[index] != null)
+            boardVisuals[index].SetActive(visible);
     }
 }
+
--- Assets/Scripts/BarricadeEventArgs.cs
+++ Assets/Scripts/BarricadeEventArgs.cs
@@ -2,6 +2,54 @@ using UnityEngine;
 using System;
 public class BarricadeEventArgs : EventArgs
 {
-    public int boardIndex { get; }
+    public int BoardIndex { get; }
     public Barricade Barricade { get; }
+
+    public BarricadeEventArgs(int boardIndex, Barricade barricade)
+    {
+        BoardIndex = boardIndex;
+        Barricade = barricade;
+    }
+
+
+}
+
+public class BoardDamagedEventArgs : BarricadeEventArgs
+{
+    public float PreviousHealth { get; }
+    public float CurrentHealth { get; }
+    public float DamageTaken => PreviousHealth - CurrentHealth;
+
+public BoardDamagedEventArgs(int boardIndex, Barricade barricade,
+                                  float previousHealth, float currentHealth)
+        : base(boardIndex, barricade)
+    {
+        PreviousHealth = previousHealth;
+        CurrentHealth = currentHealth;
+    }
+}
+
+public class BoardRepairedEventArgs : BarricadeEventArgs
+{
+    public float HealthRestored { get; }
+    public bool WasFullyRepaired { get; }
+
+    public BoardRepairedEventArgs(int boardIndex, Barricade barricade,
+                                   float healthRestored, bool wasFullyRepaired)
+        : base(boardIndex, barricade)
+    {
+        HealthRestored = healthRestored;
+        WasFullyRepaired = wasFullyRepaired;
+    }
+}
+public class BarricadeBrokenEventArgs : BarricadeEventArgs
+{
+    public UnityEngine.Vector3 BreachPosition { get; }
+
+    public BarricadeBrokenEventArgs(int boardIndex, Barricade barricade,
+                                     UnityEngine.Vector3 breachPosition)
+        : base(boardIndex, barricade)
+    {
+        BreachPosition = breachPosition;
+    }
 }
