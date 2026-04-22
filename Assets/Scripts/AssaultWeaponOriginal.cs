c5bd270087b1d74e13293420b5ac8d2a16b67158
Arief Ocampo <ariefocampo@gmail.com>
2026-04-22 12:54:20 -0500

    test

--- Assets/Scripts/AssaultWeaponOriginal.cs
+++ Assets/Scripts/AssaultWeaponOriginal.cs
@@ -11,7 +11,7 @@ public class AssaultWeaponOriginal : MonoBehaviour, IBullet
     private bool isShooting = true;
     private float nextFire;
     public event EventHandler OnFire;
-    public ParticleSystem muzzleFlash;
+
 
 
     void Awake()
@@ -69,8 +69,6 @@ public class AssaultWeaponOriginal : MonoBehaviour, IBullet
             rb.AddForce(-transform.forward * BulletPower, ForceMode.Impulse);
         }
     }
-
-
 
 }
 
