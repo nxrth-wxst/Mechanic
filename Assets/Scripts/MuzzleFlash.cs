e3278152e7a151693a90902ff7b28d7815c98b36
Jurish De Leon <jurishd@live.wsd1.org>
2026-04-24 13:21:12 -0500

    privateizedmuzzleflash

--- Assets/Scripts/MuzzleFlash.cs
+++ Assets/Scripts/MuzzleFlash.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MuzzleFlash : MonoBehaviour
 {
-    public ParticleSystem muzzleFlash;  //gets the particlesystem in the gun
+    [SerializeField] private ParticleSystem muzzleFlash;  //gets the particlesystem in the script
     [SerializeField] private AssaultWeaponOriginal assaultWeaponOriginal;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
