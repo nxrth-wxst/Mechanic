dd9fc3c1cca4d1171da1faf36577643ee3d6fdc6
Jurish De Leon <jurishd@live.wsd1.org>
2026-04-28 13:25:15 -0500

    newscript

---
+++ Assets/Scripts/GunAudioFlash.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class GunAudioFlash : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem muzzleFlash;  //gets the particlesystem in the script
+    [SerializeField] private AssaultWeaponOriginal assaultWeaponOriginal;
+    [SerializeField] private PistolWeapon Pistolsound;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioSource assaultAudioSource;
+    [SerializeField] private ParticleSystem pistolFlash;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        assaultWeaponOriginal.OnFire += AssaultWeaponOriginal_OnFire;
+        Pistolsound.OnClick += PistolWeapon_OnClick;
+
+    }
+
+    private void AssaultWeaponOriginal_OnFire(object sender, System.EventArgs e)
+    {
+        
+        Debug.Log("audio");
+        assaultAudioSource.Play();
+        muzzleFlash.Play();
+    }
+
+    private void PistolWeapon_OnClick(object sender, System.EventArgs e)
+    {
+        pistolFlash.Play();
+        audioSource.Play();
+    }
+
+
+}
