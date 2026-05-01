87347c8c6b2911fc5ecba276f7a99e3e71a6ab54
HERVY PAYUMO <hervyp@live.wsd1.org>
2026-05-01 15:29:36 -0500

    wdasd

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,10 +4,12 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     private Transform target;
-    private UnityEngine.AI.NavMeshAgent basicEnemy;
-    private float updateTargetInterval = 0.2f; 
+    private NavMeshAgent basicEnemy;
+    private float updateTargetInterval = 0.2f;
     private float timer;
-    
+    private Animator animator;
+    private Barricade nearestBarricade; 
+
     private void OnTriggerEnter(Collider other)
     {
         PColliable pCollidable = other.GetComponent<PColliable>();
@@ -15,34 +17,65 @@ public class EnemyAI : MonoBehaviour
         {
             pCollidable.PlayerCollision(this);
         }
-    
-    
-    
     }
-  
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        basicEnemy = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        if (Barricade.Instance != null)
-        {
-            target = Barricade.Instance.getNavmeshTarget();
-        }
-        else Debug.LogWarning("No Barricade instance found!", this);
+        animator = GetComponent<Animator>();
+        basicEnemy = GetComponent<NavMeshAgent>();
+        UpdateNearestBarricade();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         timer += Time.deltaTime;
         if (timer >= updateTargetInterval)
         {
-            basicEnemy = GetComponent<NavMeshAgent>();
             timer = 0f;
+            UpdateNearestBarricade();
+
+            if (target != null)
+                basicEnemy.SetDestination(target.position);
+        }
+
+        
+        if (nearestBarricade != null && nearestBarricade.EmyNearBarricade)
+        {
+            animator.SetBool("BarricadeAttack", true);
+        }
+        else
+        {
+            animator.SetBool("BarricadeAttack", false); 
         }
     }
 
+    private void UpdateNearestBarricade()
+    {
+        Barricade[] allBarricades = FindObjectsByType<Barricade>(FindObjectsSortMode.None);
 
+        if (allBarricades.Length == 0)
+        {
+            Debug.LogWarning("No Barricades found in scene!", this);
+            return;
+        }
 
+        Barricade nearest = null;
+        float shortestDistance = Mathf.Infinity;
+
+        foreach (Barricade barricade in allBarricades)
+        {
+            float distance = Vector3.Distance(transform.position, barricade.transform.position);
+            if (distance < shortestDistance)
+            {
+                shortestDistance = distance;
+                nearest = barricade;
+            }
+        }
+
+        if (nearest != null)
+        {
+            nearestBarricade = nearest; 
+            target = nearest.getNavmeshTarget();
+        }
+    }
 }
