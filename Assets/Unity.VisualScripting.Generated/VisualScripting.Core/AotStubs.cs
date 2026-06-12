c1b06b02daea13e8fc4ccb4c648f411ebd868f21
Jurish De Leon <jurishd@live.wsd1.org>
2026-06-12 13:55:39 -0500

    weifuhdy

--- Assets/Unity.VisualScripting.Generated/VisualScripting.Core/AotStubs.cs
+++ Assets/Unity.VisualScripting.Generated/VisualScripting.Core/AotStubs.cs
@@ -1090,6 +1090,7 @@ namespace Unity.VisualScripting.Generated.Aot
 	// --------------- Scene Stubs
 	// --------------- Scene Stubs
 	// --------------- Scene Stubs
+	// --------------- Scene Stubs
 	[UnityEngine.Scripting.PreserveAttribute()]
 	public class AotStubs
 	{
